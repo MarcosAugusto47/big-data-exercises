f11f7b5127599a70016cd146629979c476079c3f
MarcosAugusto47 <marcosbsb2014@gmail.com>
2022-11-24 22:18:48 -0300

    some code for the question 2

--- L1/L1-resolucao.R
+++ L1/L1-resolucao.R
@@ -48,16 +48,55 @@ teste = ac1 %>% filter(vacina_nome=='COVID-19 JANSSEN - Ad26.COV2.S')
 #                          "sg_iata_origem", "nm_aerodromo_origem", "sg_iata_destino",
 #                          "nm_aerodromo_destino", "nr_passag_pagos", "nr_passag_gratis")
 
-janssen = vroom(file = pipe("findstr JANSSEN dados\\AC1.csv"), 
-                locale = locale("br", encoding = "latin1"))
+janssen = vroom(file = pipe("findstr JANSSEN dados\\AC1.csv"),
+                delim = ";",
+                locale = locale("br", encoding = "UTF-8"))
 
 janssen_memory_R = object.size(janssen)
 (ac1_memory_R - janssen_memory_R)
 
 ## E
-janssen_all = vroom(file = pipe("findstr JANSSEN dados\\*.csv"), 
-                    locale = locale("br", encoding = "latin1"))
+janssen_all = vroom(file = pipe("findstr JANSSEN dados\\*.csv"),
+                    delim = ";",
+                    locale = locale("br", encoding = "UTF-8"))
 
 # 2
+## A
 library(data.table)
 library(geobr)
+files = list.files(path='dados/', full.names = TRUE)
+COLS = c('estabelecimento_uf', 'vacina_descricao_dose', 'estabelecimento_municipio_codigo')
+covid_subset = rbindlist(lapply(files, fread, select = COLS))
+
+health_region = read_health_region()
+
+municipal_code = read.csv("Tabela_codigos.csv")
+colnames(municipal_code) = c('x', 'uf', 'municipio', 'cod_IBGE', 'cod_regiao_saude', 'nome_regiao_saude')
+
+## B
+covid_subset_add = merge(covid_subset, municipal_code,
+                     by.x = 'estabelecimento_municipio_codigo',
+                     by.y = 'cod_IBGE',
+                     all.x = TRUE)
+
+agg_region_vaccinated = covid_subset_add[, .N, by = nome_regiao_saude]
+vaccination_median = median(agg_region_vaccinated$N)
+agg_region_vaccinated = agg_region_vaccinated[, classification := ifelse (N <= vaccination_median, "Baixa", "Alta")]
+agg_region_vaccinated = agg_region_vaccinated[order(N), ]
+
+agg_region_vaccinated[classification == "Baixa"][1:5, ]
+agg_region_vaccinated[classification == "Alta"][1:5, ]
+
+## C
+library(dplyr)
+library(dtplyr)
+lazy_covid_subset = lazy_dt(covid_subset)
+lazy_agg_region_vaccinated = lazy_covid_subset %>% 
+                              left_join(municipal_code, by = c("estabelecimento_municipio_codigo" = "cod_IBGE")) %>% 
+                              group_by(nome_regiao_saude) %>% 
+                              summarise(n = n()) %>% 
+                              mutate(classification = if_else(n <= median(n), "Baixa", "Alta")) %>% 
+                              arrange(n)
+
+## D
+
